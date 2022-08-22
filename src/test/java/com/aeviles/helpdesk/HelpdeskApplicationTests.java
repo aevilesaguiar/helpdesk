99c74d9395dd9afb340cdd1a4856ae94e59b64cd
aevilesaguiar <aeviles@hotmail.com>
2022-08-22 11:16:40 -0300

    Initial commit

---
+++ src/test/java/com/aeviles/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeviles.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
